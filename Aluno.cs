498fa278f6a7bef5e7819dea9f65c8e780e3344a
Weslley Alberto <weslleyalberto@gmail.com>
2022-11-30 19:54:49 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,16 @@
+﻿namespace Tester.Domain
+{
+    public class Aluno : Entity
+    {
+        public string Nome { get; private set; }
+        public int Idade { get; private set; }
+        protected Aluno() { }
+        public Aluno(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+        public void AlterarNome(string nome) => Nome = nome;
+        public void AlterarIDade(int idade) => Idade = idade;
+    }
+}
---
+++ Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Tester.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; private set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
